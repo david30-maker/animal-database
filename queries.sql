38a728ae5cb2f272aeb69cdc602b6485f5feb44e
David Igbo <95672514+david30-maker@users.noreply.github.com>
2023-05-15 21:50:32 +0100

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,3 +1,7 @@
 /*Queries that provide answers to the questions from all projects.*/
-
-SELECT * from animals WHERE name = 'Luna';
+SELECT * FROM animals name LIKE '%mon';
+SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-13';
+SELECT name FROM animale WHERE neutered = true AND escape_attempts < 3;
+SELECT name, escape_attempts FROM animals WHER weight_kg > 10.5;
+SELECT * FROM animals WHERE name <> 'Gabumon';
+SELECT * FROM animals weight_kg BETWEEN 10.4 AND 17.3;
