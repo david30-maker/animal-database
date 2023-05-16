fb9eb1689b7c79275678d9ac4c1080d2abc4a3b1
David Igbo <95672514+david30-maker@users.noreply.github.com>
2023-05-16 22:32:12 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,9 +1,9 @@
 CREATE TABLE animals (
- id SERIAL PRIMARY KEY,
-  name VARCHAR(255),
-  date_of_birth DATE,
-  escape_attempts INTEGER,
-  neutered BOOLEAN,
-  weight_kg DECIMAL(5, 2),
-	species VARCHAR(100)
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  date_of_birth DATE NOT NULL,
+  escape_attempts INTEGER NOT NULL,
+  neutered BOOLEAN NOT NULL,
+  weight_kg DECIMAL(5, 2) NOT NULL,
+  species VARCHAR(255) NOT NULL
 );
