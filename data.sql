2d59621fb5539c0cc15cfae5433a308d1031a508
David Igbo <95672514+david30-maker@users.noreply.github.com>
2023-05-25 11:23:05 +0100

    Update data.sql

--- data.sql
+++ data.sql
@@ -88,3 +88,9 @@ values (1, 1, '2020-5-24'),
 (6, 2, '2020-8-3'),
 (10, 3, '2020-5-24'),
 (10, 1, '2021-1-11');
+
+INSERT INTO visits (animal_id, vet_id, visit_date) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, 
+generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+
+insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000),
+'owner_' || generate_series(1,2500000) || '@mail.com';
